6e77715bc9144593e8f38b64e155ce65a21c5676
Pablo Insua <pablo.insua@gmail.com>
2023-04-10 14:44:09 +0200

    The audio file path is now a command argument. The api key is now in a config.properties file (the user will have to create this file with their own api key)

--- src/main/java/eu/nevian/speech_to_text_simple_java_client/Main.java
+++ src/main/java/eu/nevian/speech_to_text_simple_java_client/Main.java
@@ -1,14 +1,40 @@
 package eu.nevian.speech_to_text_simple_java_client;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
-    private final static String apiKey = "YOUR_API_KEY";
-    private final static String audioFilePath = "THE_PATH_TO_YOUR_AUDIO_FILE";
-
     public static void main(String[] args) {
         System.out.println("Welcome!");
 
+        if (args.length < 1) {
+            System.out.println("Usage: java -jar <audio_file_path>");
+            System.exit(1);
+        }
+
+        String audioFilePath = args[0];
+
+        Properties properties = new Properties();
+        try (FileInputStream fileInputStream = new FileInputStream("config.properties")) {
+            properties.load(fileInputStream);
+        } catch (FileNotFoundException e) {
+            System.err.println("Unable to find config.properties file.");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Error reading config.properties file.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        String apiKey = properties.getProperty("api_key");
+        if (apiKey == null) {
+            System.err.println("API key not found in config.properties file.");
+            System.exit(1);
+        }
+
+        System.out.println("API key: " + apiKey);
         ApiService apiService = new ApiService();
 
         try {
