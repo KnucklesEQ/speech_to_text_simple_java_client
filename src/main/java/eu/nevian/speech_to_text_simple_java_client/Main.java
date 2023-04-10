822620fb06ac10bda3597e27f23c8fc9cf62f672
Pablo Insua <pablo.insua@gmail.com>
2023-04-10 20:09:56 +0200

    If the file passed as argument is a video file, we extract the audio with ffmpeg (ONLY IN LINUX)

--- src/main/java/eu/nevian/speech_to_text_simple_java_client/Main.java
+++ src/main/java/eu/nevian/speech_to_text_simple_java_client/Main.java
@@ -16,32 +16,56 @@ public class Main {
         System.out.println("Welcome!");
 
         if (args.length < 1) {
-            System.out.println("Usage: java -jar <audio_file_path>");
+            System.out.println("Usage: java -jar speech_to_text_simple_java_client.jar <audio_file_path>");
             System.exit(1);
         }
 
-        String audioFilePath = args[0];
+        String filePath = args[0];
+        String fileType = validateFile(filePath);
+        String apiKey = loadApiKey();
 
-        Path filePath = Paths.get(audioFilePath);
+        if (fileType.equals("video")) {
+            try {
+                filePath = extractAudioFromVideo(filePath);
+            } catch (IOException e) {
+                System.err.println("Error extracting audio from video: " + e.getMessage());
+                System.exit(1);
+            }
+        }
+
+        ApiService apiService = new ApiService();
+
+        try {
+            System.out.println();
+            System.out.println("###### Trying access to OpenAI API ######");
+            String responseText = apiService.checkWhisperOpenAiModel(apiKey);
+            System.out.println("API Response: ");
+            System.out.println(responseText);
+
+            System.out.println();
+            System.out.println("###### Transcribe audio to text ######");
+            //String audioTranscription = apiService.transcribeAudioFile(apiKey, filePath);
+            System.out.println("Text: ");
+            //System.out.println(audioTranscription);
+        } catch (IOException e) {
+            System.err.println("Error fetching data from API: " + e.getMessage());
+        }
+    }
+
+    private static String validateFile(String filePath) {
+        final Path path = Paths.get(filePath);
 
         // Check if the file exists
-        if (!Files.exists(filePath)) {
-            System.err.println("File not found: " + audioFilePath);
+        if (!Files.exists(path)) {
+            System.err.println("File not found: " + filePath);
             System.exit(1);
         }
 
         // Check the file type
-        Tika tika = new Tika();
-        String fileType = "";
+        final Tika tika = new Tika();
 
-        try {
-            String mimeType = tika.detect(filePath);
-            MediaType mediaType = MediaType.parse(mimeType);
-            fileType = mediaType.getType();
-        } catch (IOException e) {
-            System.err.println("Error detecting file type: " + e.getMessage());
-            System.exit(1);
-        }
+        MediaType mediaType = MediaType.parse(tika.detect(filePath));
+        String fileType = mediaType.getType();
 
         if (!fileType.equals("audio") && !fileType.equals("video")) {
             System.err.println("Invalid file type. Please provide an audio or video file.");
@@ -50,7 +74,12 @@ public class Main {
 
         System.out.println("File type: " + fileType);
 
+        return fileType;
+    }
+
+    private static String loadApiKey() {
         Properties properties = new Properties();
+
         try (FileInputStream fileInputStream = new FileInputStream("config.properties")) {
             properties.load(fileInputStream);
         } catch (FileNotFoundException e) {
@@ -68,22 +97,27 @@ public class Main {
             System.exit(1);
         }
 
-        ApiService apiService = new ApiService();
+        return apiKey;
+    }
+
+    private static String extractAudioFromVideo(String videoFilePath) throws IOException {
+        String audioFilePath = videoFilePath.replaceFirst("[.][^.]+$", "") + ".mp3";
+
+        ProcessBuilder processBuilder = new ProcessBuilder("ffmpeg", "-i", videoFilePath, "-vn", "-acodec", "libmp3lame", audioFilePath);
+        Process process = processBuilder.start();
 
         try {
-            System.out.println();
-            System.out.println("###### Trying access to OpenAI API ######");
-            String responseText = apiService.checkWhisperOpenAiModel(apiKey);
-            System.out.println("API Response: ");
-            System.out.println(responseText);
-
-            System.out.println();
-            System.out.println("###### Transcribe audio to text ######");
-            //String audioTranscription = apiService.transcribeAudioFile(apiKey, audioFilePath);
-            System.out.println("Text: ");
-            //System.out.println(audioTranscription);
-        } catch (IOException e) {
-            System.err.println("Error fetching data from API: " + e.getMessage());
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new IOException("Error extracting audio from video: ffmpeg exit code " + exitCode);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IOException("Error extracting audio from video: ffmpeg process was interrupted", e);
         }
+
+        return audioFilePath;
     }
+
+
 }
