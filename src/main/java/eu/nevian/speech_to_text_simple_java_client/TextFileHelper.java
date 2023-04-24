9c0f02c4b781075e79385b336eafaf1a6b3b40a2
Pablo Insua <pablo.insua@gmail.com>
2023-04-24 23:01:11 +0200

    feat: added a new method to move a file to another folder in the text file helper class. We will use it with the transcription.txt file

--- src/main/java/eu/nevian/speech_to_text_simple_java_client/TextFileHelper.java
+++ src/main/java/eu/nevian/speech_to_text_simple_java_client/TextFileHelper.java
@@ -1,13 +1,22 @@
 package eu.nevian.speech_to_text_simple_java_client;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class TextFileHelper {
-    private TextFileHelper() {}
+    private TextFileHelper() {
+    }
 
     public static void saveTranscriptionToFile(String transcription, String outputFilePath) throws IOException {
         Files.writeString(Path.of(outputFilePath), transcription);
+    }
+
+    public static void moveTranscriptionFile(File sourceFile, String destinationFolder, String destinationFileName) throws IOException {
+        File destinationFile = new File(destinationFolder, destinationFileName);
+
+        Files.move(sourceFile.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 }
